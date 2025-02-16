80c8cdc6aec39289d71d8aa34e79f989a29ca8eb
metold814 <metold814@gmail.com>
2025-02-16 22:42:11 +0100

    first game complete

--- main.py
+++ main.py
@@ -1,24 +1,58 @@
 # this allows us  to use code from 
 # the open-source pygame library 
 # thoughout this file 
-import pygame 
 # import the connect_database function and the database_version variable from database.py 
 # from database import connect_database, database_version 
+import sys
+import pygame 
 from constants import *
-
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
+    
     pygame.init()        
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock() 
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = updatable
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) 
+
+    dt = 0 
+   
     while True:
           for event in pygame.event.get():
             if event.type == pygame.QUIT:
                  return
+            updatable.update(dt)
+            for asteroid in asteroids:
+                 if asteroid.collides_with(player):
+                      print("Game over!")
+                      sys.exit()
+                 for shot in shots:
+                      if asteroid.collides_with(shot):
+                           shot.kill()
+                           asteroid.split()
 
-            
             screen.fill("black")
+
+            for obj in drawable:
+               obj.draw(screen)
+               
             pygame.display.flip()
+            # limit the framerate to 60 fps 
+            dt = clock.tick(60) / 1000 
 
 
 
